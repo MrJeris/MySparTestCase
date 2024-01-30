17f2da935875ea744687c4948da9a180b9e10e59
MrJeris <mrjeris@yandex.ru>
2024-01-30 17:17:49 +0300

    Initial Commit

---
+++ MySparTestCase/MySparTestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparTestCaseApp.swift
+//  MySparTestCase
+//
+//  Created by Ruslan Magomedov on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparTestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
